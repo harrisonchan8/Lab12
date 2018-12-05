072070f41d767255130280ca86a2d6f79e79e45b
harrisonchan <hc8@illinois.edu>
2018-12-05 17:37:54 -0600

    Added APICaller

--- app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.illinois.cs.cs125.lab12;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -14,8 +16,19 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Main screen for our API testing app.
@@ -24,20 +37,14 @@ import android.widget.Toast;
 
 public final class MainActivity extends AppCompatActivity {
 
-    /**
-     * wattagePerHour
-     * usedPerYear
-     * energyUsePerYear
-     * costPerYear
-     *
-     * applianceArray
-     */
-
     /** Default logging tag for messages from the main activity. */
     private static final String TAG = "Lab12:Main";
 
     /** Request queue for our network requests. */
     private static RequestQueue requestQueue;
+
+    final String apiKey = "ZNZ0lwTSjSn3ECYTuSw92smDiAJTgmP5CTl2Me46";
+    final String uri = "https://developer.nrel.gov/api/utility_rates/v3.json?";
 
     /**
      * Run when our activity comes into view.
@@ -47,53 +54,50 @@ public final class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Set up a queue for our Volley requests
         requestQueue = Volley.newRequestQueue(this);
-
-        // Load the main layout for our activity
-        setContentView(R.layout.activity_main);
-
-        // Attach the handler to our UI button
-        final Button startAPICall = findViewById(R.id.testButton);
-        startAPICall.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                Log.d(TAG, "Start API button clicked");
-                startAPICall();
+        setContentView(R.layout.fuck_cs125);
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
             }
-        });
-
-
-        // Make sure that our progress bar isn't spinning and style it a bit
-        ProgressBar progressBar = findViewById(R.id.progressBar);
-        progressBar.setVisibility(View.INVISIBLE);
+            CalculatorSlides calculatorSlidesFragment = new CalculatorSlides();
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, calculatorSlidesFragment, "calculatorSlidesFragment").commit();
+        }
+        //dummy query
+        String query = "&lat=35.45&lon=-82.98";
+        apiCall(query);
     }
 
-    /**
-     * Make an API call.
-     */
-    void startAPICall() {
+    void apiCall(String query) {
         try {
+            String url = uri + "api_key=" + apiKey + query;
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                    Request.Method.GET,
-                    "",
-                    null,
+                    Request.Method.GET, url, null,
                     new Response.Listener<JSONObject>() {
                         @Override
                         public void onResponse(final JSONObject response) {
-                            Log.d(TAG, response.toString());
+//                            System.out.println(response);
+                            try {
+                                JSONObject outputs = response.getJSONObject("outputs");
+                                Double commercial = outputs.getDouble("commercial");
+                                Double residential = outputs.getDouble("residential");
+                                Double[] costArr = {commercial, residential};
+                                for (int i = 0; i < costArr.length; i++) {
+                                    System.out.println(costArr[i]);
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
                         }
                     }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(final VolleyError error) {
-                            Log.w(TAG, error.toString());
-                        }
-                    });
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    System.out.println(error.toString());
+                }
+            });
             requestQueue.add(jsonObjectRequest);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
