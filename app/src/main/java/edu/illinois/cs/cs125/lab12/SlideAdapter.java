c94fe0926be1c2b5d61f4bd55bc5b193c375946e
harrisonchan <hc8@illinois.edu>
2018-12-05 01:18:57 -0600

    Implemented ViewPager 2.0

--- app/src/main/java/edu/illinois/cs/cs125/lab12/SlideAdapter.java
+++ app/src/main/java/edu/illinois/cs/cs125/lab12/SlideAdapter.java
@@ -1,13 +1,22 @@
 package edu.illinois.cs.cs125.lab12;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class SlideAdapter extends PagerAdapter {
     Context ctx;
@@ -21,15 +30,14 @@ public class SlideAdapter extends PagerAdapter {
 
     @Override
     public int getCount() {
-        return temporaryPageTitles.length;
+        return 2;
     }
 
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
         inflater = (LayoutInflater) ctx.getSystemService(ctx.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.calculator_slide, container, false);
-        LinearLayout slideLinearLayout = view.findViewById(R.id.calculatorSlideLinearLayout);
+        View view = inflater.inflate(R.layout.view_pager_slides, container, false);
         TextView pageTitle = view.findViewById(R.id.pageTitle);
         pageTitle.setText(temporaryPageTitles[position]);
         container.addView(view);
@@ -40,8 +48,6 @@ public class SlideAdapter extends PagerAdapter {
     public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
         container.removeView((LinearLayout) object);
     }
-
-
 
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
