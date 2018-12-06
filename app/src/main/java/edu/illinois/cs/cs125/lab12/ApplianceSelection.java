f565940712bd89844543591b5ef6b7320f462e18
harrisonchan <hc8@illinois.edu>
2018-12-05 18:16:20 -0600

    added spinner

--- app/src/main/java/edu/illinois/cs/cs125/lab12/ApplianceSelection.java
+++ app/src/main/java/edu/illinois/cs/cs125/lab12/ApplianceSelection.java
@@ -2,11 +2,14 @@ package edu.illinois.cs.cs125.lab12;
 
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.jaredrummler.materialspinner.MaterialSpinner;
 
 import java.util.List;
 
@@ -20,6 +23,16 @@ public class ApplianceSelection extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.appliance_selection, container, false);
+        MaterialSpinner spinner = (MaterialSpinner) view.findViewById(R.id.materialSpinner);
+        String[] applianceList = {"Ceiling Fan", "Clother Dryer","Clothes Wahser","Computer","Dish Waher", "Har Dryer"
+                ,"Microvate","Humidifier","Air Conditioner", "Oven", "Refrigirator","Television","Water Heater", "Vacuum"};
+        spinner.setItems(applianceList);
+        spinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<String>() {
+
+            @Override public void onItemSelected(MaterialSpinner view, int position, long id, String item) {
+                Snackbar.make(view, "Clicked " + item, Snackbar.LENGTH_LONG).show();
+            }
+        });
         return view;
     }
 
