0d2360f3b50df9a89a0fea08533382af7f996d86
admin <lmou2@illinois.edu>
2018-12-06 14:45:15 -0600

    I did changes on inputApplianceStats page

--- app/src/main/java/edu/illinois/cs/cs125/lab12/InputApplianceStats.java
+++ app/src/main/java/edu/illinois/cs/cs125/lab12/InputApplianceStats.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -16,12 +18,15 @@ public class InputApplianceStats extends Fragment {
 
     public InputApplianceStats() { }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.input_appliance_stats, container, false);
         Button getResults = view.findViewById(R.id.calculateResultsButton);
+        int wattsPerHour = Integer.parseInt(view.findViewById(R.id.Wattage);
+        int timePerDay = Integer.parseInt(view.findViewById(R.id.timesUsed));
+        TextView price = view.findViewById(R.id.priceResult);
+        int num = wattsPerHour * timePerDay * 30;
         getResults.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,6 +38,8 @@ public class InputApplianceStats extends Fragment {
                         getFragmentManager().beginTransaction().remove(fragment).commit();
                     }
                 }
+                Toast.makeText(view.getContext(), "pushed", 100).show();
+                //price.setText(num);
             }
         });
         return view;
