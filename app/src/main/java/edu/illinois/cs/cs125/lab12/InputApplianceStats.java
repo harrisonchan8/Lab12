06498e2208a2f06ad898c0142c233e3dfef56eda
harrisonchan <hc8@illinois.edu>
2018-12-06 19:03:19 -0600

    Added api callback

--- app/src/main/java/edu/illinois/cs/cs125/lab12/InputApplianceStats.java
+++ app/src/main/java/edu/illinois/cs/cs125/lab12/InputApplianceStats.java
@@ -22,6 +22,12 @@ public class InputApplianceStats extends Fragment {
     final String apiKey = "ZNZ0lwTSjSn3ECYTuSw92smDiAJTgmP5CTl2Me46";
     final String uri = "https://developer.nrel.gov/api/utility_rates/v3.json?";
     private APIBitch myApi = new APIBitch(uri, apiKey);
+    private int wattsPerHour;
+    private int timesUsedPerYear;
+    private int num;
+    private String stateSelected;
+    private Double residentialCost;
+    private Double commercialCost;
 
     public InputApplianceStats() { }
 
@@ -31,21 +37,32 @@ public class InputApplianceStats extends Fragment {
         View view = inflater.inflate(R.layout.input_appliance_stats, container, false);
         Button getResults = view.findViewById(R.id.calculateResultsButton);
         MaterialSpinner spinnerStatesSelection = (MaterialSpinner) view.findViewById(R.id.spinnerStateSelection);
-        String[] statesArr = {"Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"};
+        final String[] statesArr = {"Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"};
         spinnerStatesSelection.setItems(statesArr);
 
         EditText editTextWattage = view.findViewById(R.id.editTextWattage);
         EditText editTextTimesUsed = view.findViewById(R.id.editTextTimesUsed);
         String wattageString = editTextWattage.getText().toString();
         String timesUsedString = editTextTimesUsed.getText().toString();
-        int wattsPerHour = wattageString.equals("") ? 0 : Integer.parseInt(wattageString);
-        int timesUsedPerYear = timesUsedString.equals("") ? 0 : Integer.parseInt(timesUsedString);
-        int num = wattsPerHour * timesUsedPerYear;
+        wattsPerHour = wattageString.equals("") ? 0 : Integer.parseInt(wattageString);
+        timesUsedPerYear = timesUsedString.equals("") ? 0 : Integer.parseInt(timesUsedString);
+        num = wattsPerHour * timesUsedPerYear;
 
-        //Get Cost (Double) from api
-        String costQuery = "=" + statesArr[spinnerStatesSelection.getSelectedIndex()];
-        Double[] costArr = myApi.getNewCost(costQuery);
-        
+        //Update Selected State and get cost on state selected
+        spinnerStatesSelection.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(MaterialSpinner view, int position, long id, Object item) {
+                stateSelected = statesArr[position];
+                System.out.println("state selected: " + stateSelected);
+                String costQuery = "&address=" + stateSelected;
+                Double[] costArr = myApi.getNewCost(costQuery);
+                if (costArr != null) {
+                    residentialCost = costArr[0];
+                    commercialCost = costArr[1];
+                }
+                System.out.println("residential cost for " + stateSelected + "is: " + residentialCost);
+            }
+        });
 
         getResults.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,7 +75,7 @@ public class InputApplianceStats extends Fragment {
                         getFragmentManager().beginTransaction().remove(fragment).commit();
                     }
                 }
-                Toast.makeText(view.getContext(), "Fuck CS125", Toast.LENGTH_LONG).show();
+                Toast.makeText(view.getContext(), residentialCost.toString(), Toast.LENGTH_LONG).show();
                 //price.setText(num);
             }
         });
